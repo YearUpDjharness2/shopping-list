1d33163bb0afb02db5a319db77ec413ec515e534
Djharness <djharness2@gmail.com>
2024-09-25 15:15:45 -0400

    i wrote my first shopping list!

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -1,2 +1,23 @@
-package com.pluralsight;public class ShoppingList {
-}
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Shopping List");
+        System.out.println("--------------");
+        System.out.println("Fruit");
+        System.out.println("Veggies");
+        System.out.println("Starch");
+        System.out.println("Legume");
+        System.out.println("Dairy");
+        System.out.println("Breakfast");
+        System.out.println("Spices");
+        System.out.println("Stock");
+    }
+
+
+
+
+    }
+
+
